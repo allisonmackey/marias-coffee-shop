5e85bd0c8b8494d5e8fdcb858b04ccc3d4b0f5b7
Allison Mackey <a.mackey693@gmail.com>
2020-05-29 13:14:35 -0700

    test on #Product 'validate numericality of cost' expect test to fail

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, :cost, :country_of_origin, presence: true
+
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,4 +5,5 @@ describe Product do
   it { should validate_presence_of :name }
   it { should validate_presence_of :cost }
   it { should validate_presence_of :country_of_origin }
+  it { should validate_numericality_of :cost}
 end
