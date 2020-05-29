64b515cd7d83a894527bb09b9ba002c64f980ba7
Allison Mackey <a.mackey693@gmail.com>
2020-05-29 13:25:50 -0700

    add test on #Review spec 'should validate length of content body min: 50, max: 250' expect test to FAIL

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,4 +6,5 @@ describe Product do
   it { should validate_presence_of :cost }
   it { should validate_presence_of :country_of_origin }
   it { should validate_numericality_of :cost}
+ 
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,4 +6,5 @@ describe Review do
   it { should validate_presence_of :content_body }
   it { should validate_presence_of :rating }
   it { should validate_numericality_of :rating}
+  it { should validate_length_of(:content_body).is_at_least(50).is_at_most(250)}
 end
