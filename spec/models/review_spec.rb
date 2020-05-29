32a77ed700c3949e700ea40cc0d529f43e30f11e
Allison Mackey <a.mackey693@gmail.com>
2020-05-29 13:21:58 -0700

    add test on #Review spec 'should validate numericality of rating' expect test to FAIL

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,4 +5,5 @@ describe Review do
   it { should validate_presence_of :author }
   it { should validate_presence_of :content_body }
   it { should validate_presence_of :rating }
+  it { should validate_numericality_of :rating}
 end
