2451cec0135b2f600b171c021a751e38a5def43f
Allison Mackey <a.mackey693@gmail.com>
2020-05-29 13:44:59 -0700

    remove error message :too_long, was causing bug in testing

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe Review do
   it {should belong_to(:product)}
   it { should validate_presence_of :author }
-  it { should validate_presence_of :content_body }
   it { should validate_presence_of :rating }
+  it { should validate_presence_of :content_body }
   it { should validate_numericality_of :rating}
   it { should validate_length_of(:content_body).is_at_least(50).is_at_most(250)}
 end
