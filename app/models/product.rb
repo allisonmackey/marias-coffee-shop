b3ef26c033d5d6d785425e458f9bcfc3c3ebf6eb
Allison Mackey <a.mackey693@gmail.com>
2020-05-29 13:16:08 -0700

    add logic to pass test on #Product spec 'should validate numericality of cost'. test PASSES

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, :cost, :country_of_origin, presence: true
-
+  validates :cost, numericality: true
 end
