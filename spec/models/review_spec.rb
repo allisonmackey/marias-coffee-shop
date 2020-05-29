55c2906ed44b169cc83b2f7812e2879e86fece8e
Allison Mackey <a.mackey693@gmail.com>
2020-05-29 13:58:48 -0700

    add test on #Review spec 'should validate rating is less than or equal to 5' expect test to FAIL

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -8,4 +8,5 @@ describe Review do
   it { should validate_numericality_of :rating}
   it { should validate_length_of(:content_body).is_at_least(50).is_at_most(250)}
   it { should validate_numericality_of(:rating).is_greater_than(0)}
+  it { should validate_numericality_of(:rating).is_less_than_or_equal_to(5)}
 end
