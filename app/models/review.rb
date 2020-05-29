3f65c9eb1d5a1f6353ba1e5a4140a0cdf9d11971
Allison Mackey <a.mackey693@gmail.com>
2020-05-29 13:27:22 -0700

    add logic to pass test on #Review spec 'should validate length of content body min: 50, max: 250' test PASSES

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ApplicationRecord
   belongs_to :product
   validates :author, :content_body, :rating, presence: true
   validates :rating, numericality: true
+  validates_length_of :content_body, in: 50..250
 end
