d9b7f056313b2b883adb832ed019b649c33ed809
Allison Mackey <a.mackey693@gmail.com>
2020-05-29 14:04:38 -0700

    add test on #Product spec 'should titleize a product name before it is saved' expect test to FAIL

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,4 +6,9 @@ describe Product do
   it { should validate_presence_of :cost }
   it { should validate_presence_of :country_of_origin }
   it { should validate_numericality_of :cost}
+
+  it("titleizes a product before it's SAVED") do
+    product = Product.create({name: "this is a test"})
+    expect(product.name()).to(eq("This is a Test"))
+  end
 end
