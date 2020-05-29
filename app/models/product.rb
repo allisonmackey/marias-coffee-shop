a948812882dca4750262ca20179f96d0b1d2ced2
Allison Mackey <a.mackey693@gmail.com>
2020-05-29 14:20:36 -0700

    add logic to pass test on #Review spec 'should titleize a product name before it is saved' test PASSES

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,25 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  before_save(:titleize_product)
   validates :name, :cost, :country_of_origin, presence: true
   validates :cost, numericality: true
+  
+  private
+    def titleize_product
+      no_caps = ["a", "is", "you", "he", "she", "it", "we", "you", "me", "him", "her", "us", "my",  "his", "its", "our", "who",  "if", "how", "as"]
+      result = []
+      split_name = self.name.split(" ")
+      split_name.each do |w|
+        if no_caps.any? {|c| w == c }
+          w = w
+          result.push(w)
+        else
+        w = w.titleize
+        result.push(w)
+        end
+      end  
+      self.name = result.join(" ")
+    end
+
+
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,9 +6,11 @@ describe Product do
   it { should validate_presence_of :cost }
   it { should validate_presence_of :country_of_origin }
   it { should validate_numericality_of :cost}
+end
 
+describe Product do
   it("titleizes a product before it's SAVED") do
-    product = Product.create({name: "this is a test"})
+    product = Product.create({name: "this is a test", cost: 5, country_of_origin: "test"})
     expect(product.name()).to(eq("This is a Test"))
   end
 end
