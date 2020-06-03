1a64e9397364e7900a4a6a0ebd148c8c53e14094
Allison Mackey <a.mackey693@gmail.com>
2020-06-02 18:59:58 -0700

    add logic to test #Reviews integration test 'adds a new review' test PASSES.

--- spec/features/add_product_spec.rb
+++ spec/features/add_product_spec.rb
@@ -28,7 +28,17 @@ describe '#Products' do
     click_on 'Submit!'
     expect(page).to have_content 'Test Update'
   end
+end
 
-
-
+describe '#Reviews' do
+  it 'adds a new review' do
+    test_product = Product.create(name:"Test", cost: 6, country_of_origin: 'Test Country', id: nil)
+    visit product_path(test_product.id) 
+    click_link 'ADD REVIEW'
+    fill_in "review[author]", :with => 'Test Author'
+    fill_in "review[content_body]", :with => 'testing review body need at leat 50 characters: testing, testing.'
+    fill_in "review[rating]",  :with => 5
+    click_on 'Submit!'
+    expect(page).to have_content 'Test Author'
+  end
 end
