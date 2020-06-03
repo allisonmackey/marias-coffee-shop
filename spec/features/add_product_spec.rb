1be3c3c46f7aaaef5abb8850db2ac647cc8332a2
Allison Mackey <a.mackey693@gmail.com>
2020-06-02 19:08:00 -0700

    add logic to test #Reviews integration test 'deletes a review' test PASSES.

--- spec/features/add_product_spec.rb
+++ spec/features/add_product_spec.rb
@@ -41,4 +41,14 @@ describe '#Reviews' do
     click_on 'Submit!'
     expect(page).to have_content 'Test Author'
   end
+
+  it 'deletes a review' do   
+    test_product = Product.create(name:"Test", cost: 6, country_of_origin: 'Test Country', id: nil)
+    test_review = Review.create(author:"Test Authort", content_body: 'testing review body need at leat 50 characters: testing, testing.', rating: 4, product_id: test_product.id, id: nil)
+    visit edit_product_review_path(test_product, test_review)
+    click_link "DELETE THIS REVIEW"
+    # click_on "ok"
+    expect(page).to have_no_content(test_review)
+  end
+
 end
