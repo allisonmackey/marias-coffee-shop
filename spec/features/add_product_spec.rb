4c590ade97b2b749a30725a63772e9ef3d1d93ab
Allison Mackey <a.mackey693@gmail.com>
2020-06-02 18:53:42 -0700

    add logic to test #Products integration test 'updates a product' test PASSES.

--- spec/features/add_product_spec.rb
+++ spec/features/add_product_spec.rb
@@ -18,6 +18,17 @@ describe '#Products' do
     expect(page).to have_no_content 'Test'
   end
 
+  it 'edits a product' do   
+    test_product = Product.create(name:"Test", cost: 6, country_of_origin: 'Test Country', id: nil)
+    visit product_path(test_product.id) 
+    click_link 'EDIT PRODUCT'
+    fill_in "product[name]", :with => 'Test Update'
+    fill_in "product[cost]", :with => '100'
+    fill_in "product[country_of_origin]",  :with => 'Test Country'
+    click_on 'Submit!'
+    expect(page).to have_content 'Test Update'
+  end
+
 
 
 end
