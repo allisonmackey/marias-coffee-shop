20c67f1980c7190269bddaf106bb307d126f9481
Allison Mackey <a.mackey693@gmail.com>
2020-06-05 16:11:08 -0700

    fix bug with Review #update method, @product was undefinded

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -32,6 +32,7 @@ class ReviewsController < ApplicationController
   end
 
   def update
+    @product = Product.find(params[:product_id])
     @review = Review.find(params[:id])
     if @review.update(review_params)
       redirect_to product_path(@review.product)
