76abe5dc11fb61b405158fb87b218fe51c276ae6
Allison Mackey <a.mackey693@gmail.com>
2020-06-05 09:29:32 -0700

    configure devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #products routes
   root to: 'products#home'
   get 'home' => 'products#home'
   
   resources :products do  
     resources :reviews
   end
+
+  #devise routes
+  devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
   
 end
