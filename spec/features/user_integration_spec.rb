55720c00d7c763ad708421355b15ff6a3115f62c
Allison Mackey <a.mackey693@gmail.com>
2020-06-05 13:22:09 -0700

    add admin to database, test still failing

--- spec/features/user_integration_spec.rb
+++ spec/features/user_integration_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-# describe '#Users' do
-#   it 'allows user to sign up' do
-#     visit home_path
-#     click_link 'SIGN UP'
-#     fill_in "user[email]", :with => 'test@test.com'
-#     fill_in "user[password]", :with => 'test123'
-#     fill_in "user[password_confirmation]",  :with => 'test123'
-#     click_on 'Sign up'
-#     expect(current_user).to eq true
-#   end
-# end
+describe '#Users' do
+  it 'allows user to sign up' do
+    visit home_path
+    click_link 'SIGN UP'
+    fill_in "user[email]", :with => 'test@test.com'
+    fill_in "user[password]", :with => 'test123'
+    fill_in "user[password_confirmation]",  :with => 'test123'
+    click_on 'Sign up'
+    expect(page).to have_content 'test@test.com'
+  end
+end
