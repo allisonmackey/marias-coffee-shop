e700126feabe858b765ea9f7b257e9645bfb6312
Allison Mackey <a.mackey693@gmail.com>
2020-06-05 15:52:34 -0700

    add logic to #user integration spec 'does not allow non-user to edit/delete/review fields' test PASSES'

--- spec/features/user_integration_spec.rb
+++ spec/features/user_integration_spec.rb
@@ -9,4 +9,12 @@ describe '#Users' do
     click_on 'Sign up'
     expect(page).to have_content 'test@test.com'
   end
+
+  it 'does not allow a non-user to see edit/delete/review feilds' do
+    test_product = Product.create(name:"123 Test", cost: 6, country_of_origin: 'Test Country', id: nil)
+    visit product_path(test_product.id) 
+    expect(page).to have_no_content 'EDIT'
+    expect(page).to have_no_content 'DELETE'
+    expect(page).to have_no_content 'ADD'
+  end
 end
