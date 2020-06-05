59308c99f48c534ce11845519e1185366a2da234
Allison Mackey <a.mackey693@gmail.com>
2020-06-05 15:46:55 -0700

    add logic to pass integration test for #user 'allows user to sign up' test PASSES

--- spec/features/user_integration_spec.rb
+++ spec/features/user_integration_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-# describe '#Users' do
-#   it 'allows user to sign up' do
-#     click_link 'SIGN UP'
-#     fill_in "user[email]", :with => 'test@test.com'
-#     fill_in "user[password]", :with => 'test123'
-#     fill_in "user[password_confirmation]",  :with => 'test123'
-#     click_on 'Sign up'
-#     expect(page).to have_content 'test@test.com'
-#   end
-# end
+describe '#Users' do
+  it 'allows user to sign up' do
+    visit new_user_registration_path
+    fill_in "user[email]", :with => 'test@test.com'
+    fill_in "user[password]", :with => 'test123'
+    fill_in "user[password_confirmation]",  :with => 'test123'
+    click_on 'Sign up'
+    expect(page).to have_content 'test@test.com'
+  end
+end
