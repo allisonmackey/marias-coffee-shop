78f6c4a4ce59688b9a53f53de2c786f1f50ba44c
Allison Mackey <a.mackey693@gmail.com>
2020-06-05 16:20:23 -0700

    add logic to pass test on review int test 'edit a review' test PASSES

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,7 +13,7 @@ class ReviewsController < ApplicationController
     @product = Product.find(params[:product_id])
     @review = @product.reviews.new(review_params)
     if @review.save
-      redirect_to product_path(@product.review)
+      redirect_to product_path(@product)
     else 
       render :new
     end
--- spec/features/reviews_integration_spec.rb
+++ spec/features/reviews_integration_spec.rb
@@ -5,7 +5,7 @@ describe '#Reviews' do
     user = User.create!(:email => 'test@example.com', :password => 'f4k3p455w0rd', :admin=> true )
     login_as(user, :scope => :user)
   end
-  
+
   it 'adds a new review' do
     test_product = Product.create(name:"Test", cost: 6, country_of_origin: 'Test Country', id: nil)
     visit product_path(test_product.id) 
@@ -23,5 +23,17 @@ describe '#Reviews' do
     visit edit_product_review_path(test_product, test_review)
     click_link "DELETE THIS REVIEW"
     expect(page).to have_no_content(test_review)
+  end
+
+  it 'edits a review' do   
+    test_product = Product.create(name:"Test", cost: 6, country_of_origin: 'Test Country', id: nil)
+    test_review = Review.create(author:"Test Author", content_body: 'testing review body need at leat 50 characters: testing, testing.', rating: 4, product_id: test_product.id, id: nil)
+    visit edit_product_review_path(test_product, test_review)
+    fill_in "review[author]", :with => 'Test Update Author'
+    fill_in "review[content_body]", :with => 'testing review body need at leat 50 characters: testing, testing.'
+    fill_in "review[rating]",  :with => 5
+    click_on 'Submit!'
+    save_and_open_page
+    expect(page).to have_content("Test Update Author")
   end
 end
